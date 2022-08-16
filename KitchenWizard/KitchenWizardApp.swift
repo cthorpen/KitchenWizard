79620821d675fc03f3b34a68d9cd19e2232fb874
Cole Thorpen <thorpencole@gmail.com>
2022-08-16 15:58:36 -0700

    Initial Commit

---
+++ KitchenWizard/KitchenWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenWizardApp.swift
+//  KitchenWizard
+//
+//  Created by Cole Thorpen on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct KitchenWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
