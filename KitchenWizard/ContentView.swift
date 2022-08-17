70e4a6019613b92f8da7ef0433be1983dcf8670c
Cole Thorpen <thorpencole@gmail.com>
2022-08-17 11:14:14 -0700

    basic app layout

--- KitchenWizard/ContentView.swift
+++ KitchenWizard/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+                Text("\"What's in the Fridge?\" View")
+                    .tabItem {
+                        Label("My Fridge", systemImage: "house")
+                }
+                Text("\"Random Recipe\" Screen")
+                    .tabItem {
+                        Image(systemName: "questionmark")
+                        Text("Random")
+                }
+                Text("Search Screen")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                }
+            Text("Meal Prep Screen")
+                .tabItem {
+                    Image(systemName: "list.bullet.rectangle")
+                    Text("Meal Prep")
+            }
+            Text("\"My Recipes\" View")
+                .tabItem {
+                    Image(systemName: "books.vertical.fill")
+                    Text("My Recipes")
+            }
+            }
+        .padding()
     }
 }
 
