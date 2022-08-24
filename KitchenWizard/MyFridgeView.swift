05f12869a33c2574a00446e13bfbd8337f9d34bc
Cole Thorpen <thorpencole@gmail.com>
2022-08-24 12:21:59 -0700

    fridge view placeholder title

--- KitchenWizard/MyFridgeView.swift
+++ KitchenWizard/MyFridgeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MyFridgeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("My Fridge/Account View")
     }
 }
 
